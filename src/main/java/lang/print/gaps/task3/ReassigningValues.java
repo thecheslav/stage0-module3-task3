4cf46de3917794c601dd94722f611d51fc110abd
cheslav <v.shlapatsky@gmail.com>
2022-06-30 20:56:37 +0300

    two

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-    System.out.println("1\n10\n100\n15\n6\n4\n1\n10\n100\n");
+    System.out.println("1\n10\n100\n15\n6\n4\n1\n10\n100");
     }
 }
